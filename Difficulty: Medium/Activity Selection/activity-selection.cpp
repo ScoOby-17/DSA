d1dacc39aad693153d85a9cac9d0a8eadfef3c1b
ScoOby_17 <suhaibigl@gmail.com>
2026-06-18 22:49:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Activity Selection/activity-selection.cpp
+++ Difficulty: Medium/Activity Selection/activity-selection.cpp
@@ -2,22 +2,23 @@ class Solution {
   public:
     int activitySelection(vector<int> &start, vector<int> &finish) {
         // code here
-        vector<vector<int>>arr;
-        int maxActivity=1;
+        vector<vector<int>>jobs;
         for(int i=0;i<start.size();i++){
-            arr.push_back({start[i],finish[i]});
+            jobs.push_back({start[i],finish[i]});
         }
-        sort(arr.begin(),arr.end(), [](vector<int>&a , vector<int>&b){
+        
+        sort(jobs.begin(),jobs.end(),[](auto &a, auto &b){
             return a[1]<b[1];
         });
         
-        int prevEnd = arr[0][1];
-        for(int i=1;i<arr.size();i++){
-            if(prevEnd<arr[i][0]){
-                maxActivity++;
-                prevEnd = arr[i][1];
+        int Activity = 1;
+        int prevEnd = jobs[0][1];
+        for(int i=1;i<jobs.size();i++){
+            if(prevEnd < jobs[i][0]){
+                Activity++;
+                prevEnd = jobs[i][1];
             }
         }
-        return maxActivity;
+        return Activity;
     }
 };
