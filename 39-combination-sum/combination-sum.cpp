8a54b215fc84a7e8ae642de42381b3f249db0218
ScoOby_17 <suhaibigl@gmail.com>
2026-06-27 11:58:58 +0530

    Time: 92 ms (6.22%) | Memory: 72.1 MB (6.59%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     set<vector<int>>s;
-    void genCombination(vector<int>& arr , vector<int>combination , int target , int i){
+    void genCombination(vector<int>& arr , vector<int>combination , int target , int i , vector<vector<int>> &ans){
         //base cases
         if(target < 0 || i == arr.size()) return;
         if(target == 0){
-            s.insert(combination);
+            ans.push_back(combination);
             return;
         }
 
         combination.push_back(arr[i]);
-        // genCombination(arr , combination , target-arr[i] , i+1); //include one time
-        genCombination(arr , combination , target-arr[i] , i); //include many time
+        // genCombination(arr , combination , target-arr[i] , i+1 , ans); //include one time
+        genCombination(arr , combination , target-arr[i] , i , ans); //include many time
         combination.pop_back(); //backtrack
-        genCombination(arr , combination , target , i+1); //exclude case
+        genCombination(arr , combination , target , i+1 , ans); //exclude case
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
         vector<int>combination;
-        genCombination(candidates , combination , target , 0);
-        vector<vector<int>>ans(s.begin(),s.end());
+        genCombination(candidates , combination , target , 0 , ans);
         return ans;
     }
 };
